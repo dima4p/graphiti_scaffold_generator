64d922dbf15c330686500b081edca1187dcdf9d9
Dmitry Kulikov <dima@koulikoff.ru>
2023-11-16 17:43:52 +0100

    bundle gem graphiti_scaffold_generator

---
+++ lib/graphiti_scaffold_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "graphiti_scaffold_generator/version"
+
+module GraphitiScaffoldGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
